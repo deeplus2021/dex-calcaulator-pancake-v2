7424982cdbf006333c8f7404389cc01b416f0f76
biginfo2012 <biginfo2012@gmail.com>
2024-05-23 19:49:57 +0300

    modified based on updated requirements

--- endpoint.ts
+++ endpoint.ts
@@ -5,7 +5,7 @@ let artifact = require('./artifacts/PancakeCal.json');
 dotenv.config();
 const web3 = new Web3(process.env.BSC_MAINNET_RPC_URL);
 const address = process.env.PAN_CONTRACT_ADDRESS;
-const DENOMINATOR = Number(process.env.DENOMINATOR);
+const DENOMINATOR = Math.pow(10, Number(process.env.DENOMINATOR));
 const contract = new web3.eth.Contract(artifact.abi, address);
 
 export async function swapableTokenAmountInThePool(pool: string, startPrice: number, endPrice: number, rangeType: number) {
@@ -14,27 +14,36 @@ export async function swapableTokenAmountInThePool(pool: string, startPrice: num
             'current': number,
             'startReserve': number,
             'endReserve': number,
-            'decimals0': number,
+            'decimals': number,
             'symbol0': string,
             'symbol1': string
         } = await contract.methods.getSwapableTokenAmount(pool, startPrice * DENOMINATOR, endPrice * DENOMINATOR, rangeType).call();
 
-        let amountForStart = (Number(returns.startReserve) - Number(returns.current)) / (10 ** Number(returns.decimals0));
-        let endForStart = (Number(returns.endReserve) - Number(returns.current)) / (10 ** Number(returns.decimals0));
+        let amountForStart = (Number(returns.startReserve) - Number(returns.current)) / (10 ** Number(returns.decimals));
+        let endForStart = (Number(returns.endReserve) - Number(returns.current)) / (10 ** Number(returns.decimals));
 
-        // get 
+        let symbol0: string;
+        let symbol1: string;
+        if (rangeType == 0) {
+            symbol0 = returns.symbol0;
+            symbol1 = returns.symbol1;
+        } else {
+            symbol1 = returns.symbol0;
+            symbol0 = returns.symbol1;
+        }
+
         if (amountForStart > 0) {
-            console.log(`${amountForStart} ${returns.symbol0} is swapable to get ${returns.symbol1} in the bottom price of ${startPrice} ${returns.symbol0}/${returns.symbol1}`);
+            console.log(`${amountForStart} ${symbol0} is swapable to get ${symbol1} in the bottom price of ${startPrice} ${symbol0}/${symbol1}`);
         } else {
             amountForStart = Math.abs(amountForStart);
-            console.log(`${amountForStart} ${returns.symbol0} is able to get by swapping ${returns.symbol1} in the bottom price of ${startPrice} ${returns.symbol0}/${returns.symbol1}`);
+            console.log(`${amountForStart} ${symbol0} is able to get by swapping ${symbol1} in the bottom price of ${startPrice} ${symbol0}/${symbol1}`);
         }
 
         if (endForStart > 0) {
-            console.log(`${endForStart} ${returns.symbol0} is swapable to get ${returns.symbol1} in the top price of ${endPrice} ${returns.symbol0}/${returns.symbol1}`);
+            console.log(`${endForStart} ${symbol0} is swapable to get ${symbol1} in the top price of ${endPrice} ${symbol0}/${symbol1}`);
         } else {
             endForStart = Math.abs(endForStart);
-            console.log(`${endForStart} ${returns.symbol0} is able to get by swapping ${returns.symbol1} in the top price of ${endPrice} ${returns.symbol0}/${returns.symbol1}`);
+            console.log(`${endForStart} ${symbol0} is able to get by swapping ${symbol1} in the top price of ${endPrice} ${symbol0}/${symbol1}`);
         }
     } catch(error) {
         console.error("Error fetching token price:", error);
